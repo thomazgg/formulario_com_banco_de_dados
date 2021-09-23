4d2b15215f15edeb9f88771c8c2651cfc269aacf
Thomaz <79727201+thomazgg@users.noreply.github.com>
2021-09-23 05:32:17 -0300

    Update excluir.php

--- excluir.php
+++ excluir.php
@@ -1,20 +1,19 @@
 <?php
-	include 'header.php'
+	include '../assets/header.php'
 ?>
-	<!-- Styles -->
-	<link rel="stylesheet" type="text/css" href="style2.css">
-</head>
 
 <body>
 	<?php 
 		if(isset($_GET["excluir"])){
 			$idcli = htmlentities($_GET["excluir"]);
-			require("conecta.php");
+			require("../conecta.php");
 			$mysqli->query("delete from tb_clientes where idcli = '$idcli'");
 			echo $mysqli->error;
 			if ($mysqli->error==""){
-				echo "Excluido com Sucesso<br />";
-				echo "<a href='index.php'>Voltar</a>";
+				echo "<div class='alerta sucesso'><b>Excluido</b> com Sucesso.
+				<a href='../index.php'>
+				<input type='button' value='Voltar'>
+				</a></div><br />";
 			}
 		}
 	?>
